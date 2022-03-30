01f08dd6840f9316570815c1f33bc91dac1e57d2
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-30 14:37:32 +0800

    chore: throw forgot page into app

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ForgotPassword from './src/screens/ForgotPassword';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 
 export default function App() {
-  return <Register />;
+  return <ForgotPassword />;
 }
