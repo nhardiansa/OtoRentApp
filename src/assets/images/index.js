084f6e3a2ab0ae14b45ddde6098ae432c7afd8d9
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-30 21:53:56 +0800

    chore: image placeholder and banner in entrypoint

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,9 @@
 export const LOGIN_BG = require('./login-bg.png');
 export const REGISTER_BG = require('./register-bg.png');
 export const FORGOT_PASSWORD_BG = require('./reset-verify-bg.png');
+
+export const HOME_BANNER = require('./home-banner.png');
+
+export const MOTORBIKE_PLACEHOLDER = require('./motorbike-placeholder.png');
+export const BIKE_PLACEHOLDER = require('./bike-placeholder.png');
+export const CAR_PLACEHOLDER = require('./car-placeholder.png');
