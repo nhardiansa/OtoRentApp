50898e8c0e65d5da38b6748fcabdc3d5ce15ad24
Nabil Hardiansa <nhardiansa@gmail.com>
2022-04-01 16:47:28 +0800

    feat: add search list display

--- src/screens/Search/SearchResult.js
+++ src/screens/Search/SearchResult.js
@@ -1,9 +1,21 @@
 import React, {useState} from 'react';
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
 
-import {Container, Box, Input, Center} from 'native-base';
+import {
+  Container,
+  Box,
+  Input,
+  Text,
+  Center,
+  Pressable,
+  FlatList,
+} from 'native-base';
 import {globalStyle} from '../../helpers/globalStyle';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
+import {colors} from '../../helpers/styleConstants';
+import {MOTORBIKE_PLACEHOLDER} from '../../assets/images';
+import ItemCard from '../../components/ItemCard';
+import {VEHICLE_DETAIL} from '../../helpers/destinationConstants';
 
 export default function SearchResult({navigation}) {
   const [search, setSearch] = useState('');
@@ -16,6 +28,60 @@ export default function SearchResult({navigation}) {
     setSearch('');
   };
 
+  const goToFilter = () => {
+    console.log('goToFilter');
+  };
+
+  const goToDetail = () => {
+    console.log('Go to Detail');
+    navigation.navigate(VEHICLE_DETAIL);
+  };
+
+  const data = [
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+    {
+      image: MOTORBIKE_PLACEHOLDER,
+      location: 'Makassar',
+      price: 250000,
+      title: 'Vespa Matic',
+      isAvailable: true,
+    },
+  ];
+
   const styles = StyleSheet.create({
     searchIcon: {
       marginLeft: 16,
@@ -25,10 +91,22 @@ export default function SearchResult({navigation}) {
       marginRight: 16,
       fontSize: 16,
     },
+    searchWrapper: {
+      borderBottomColor: '#F5F5F5',
+    },
+    filterIcon: {
+      fontSize: 20,
+      color: colors.gray,
+    },
   });
   return (
     <SafeAreaView>
-      <Box w="100%" py={7} style={[globalStyle.container]}>
+      <Box
+        style={styles.searchWrapper}
+        borderBottomWidth={1}
+        w="100%"
+        px="5"
+        py={7}>
         <Input
           w="100%"
           value={search}
@@ -44,6 +122,25 @@ export default function SearchResult({navigation}) {
               />
             ) : null
           }
+        />
+      </Box>
+      <Box px="5" borderBottomColor="#F5F5F5" borderBottomWidth={1} pb="3">
+        <Pressable onPress={goToFilter}>
+          <Box flexDir="row" alignItems="center">
+            <FAIcon name="filter" style={styles.filterIcon} />
+            <Text color={colors.grayDark} fontSize="md" ml="1.5">
+              Filter
+            </Text>
+          </Box>
+        </Pressable>
+      </Box>
+      <Box px="5">
+        <FlatList
+          data={data}
+          renderItem={({item}) => {
+            return <ItemCard onPress={goToDetail} mb="4" image={item.image} />;
+          }}
+          showsVerticalScrollIndicator={false}
         />
       </Box>
     </SafeAreaView>
