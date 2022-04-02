10113be580202f59111f2242bc8471bd2d82f1fb
Nabil Hardiansa <nhardiansa@gmail.com>
2022-04-02 15:21:17 +0800

    feat: add actionsheet on form

--- src/screens/Payment/PaymentForm.js
+++ src/screens/Payment/PaymentForm.js
@@ -1,14 +1,16 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import {Box, Input, Pressable, Select} from 'native-base';
+import {Actionsheet, Box, Input, Pressable, Select} from 'native-base';
 
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import CustomButton from '../../components/CustomButton';
 import Stepper from '../../components/Stepper';
 import {GET_PAYMENT_CODE} from '../../helpers/destinationConstants';
+import {colors} from '../../helpers/styleConstants';
 
 export default function PaymentForm({navigation}) {
   const [paymentMethod, setPaymentMethod] = React.useState('');
+  const [showPaymentMethod, setShowPaymentMethod] = React.useState(false);
 
   const styles = StyleSheet.create({
     dropDownIcon: {
@@ -22,29 +24,59 @@ export default function PaymentForm({navigation}) {
     navigation.navigate(GET_PAYMENT_CODE);
   };
 
+  const seePaymentMethod = () => {
+    console.log('See Payment Method');
+    setShowPaymentMethod(true);
+  };
+
   return (
-    <Box
-      px="5"
-      pb="6"
-      pt="12"
-      flex={1}
-      backgroundColor="white"
-      justifyContent="space-between">
-      <Box>
-        <Stepper mb="12" count={3} currentlyActive={1} />
-        <Input placeholder="Name" />
-        <Input placeholder="Email" mt={4} />
-        <Input placeholder="Phone" mt={4} />
-        <Pressable mt={4}>
-          <Input
-            placeholder="Payment Method"
-            InputRightElement={
-              <FAIcon name="caret-down" style={styles.dropDownIcon} />
-            }
-          />
-        </Pressable>
+    <>
+      <Box
+        px="5"
+        pb="6"
+        pt="12"
+        flex={1}
+        backgroundColor="white"
+        justifyContent="space-between">
+        <Box>
+          <Stepper mb="12" count={3} currentlyActive={1} />
+          <Input placeholder="Name" />
+          <Input placeholder="Email" mt={4} />
+          <Input placeholder="Phone" mt={4} />
+          <Pressable onPress={seePaymentMethod} mt={4}>
+            <Input
+              isDisabled={true}
+              bgColor={colors.gray}
+              placeholder="Payment Method"
+              InputRightElement={
+                <FAIcon name="caret-down" style={styles.dropDownIcon} />
+              }
+            />
+          </Pressable>
+        </Box>
+        <CustomButton onPress={getPaymentCode}>See Order Details</CustomButton>
       </Box>
-      <CustomButton onPress={getPaymentCode}>See Order Details</CustomButton>
-    </Box>
+      <Actionsheet
+        isOpen={showPaymentMethod}
+        onClose={() => setShowPaymentMethod(false)}>
+        <Actionsheet.Content>
+          <Box w="100%" h={60} px={4} justifyContent="center">
+            <Text
+              fontSize="16"
+              color="gray.500"
+              _dark={{
+                color: 'gray.300',
+              }}>
+              Albums
+            </Text>
+          </Box>
+          <Actionsheet.Item>Delete</Actionsheet.Item>
+          <Actionsheet.Item>Share</Actionsheet.Item>
+          <Actionsheet.Item>Play</Actionsheet.Item>
+          <Actionsheet.Item>Favourite</Actionsheet.Item>
+          <Actionsheet.Item>Cancel</Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </>
   );
 }
