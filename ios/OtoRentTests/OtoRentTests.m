8c733ef0a51cbe925ee594e0c1cd125a18046b42
Nabil Hardiansa <nhardiansa@gmail.com>
2022-04-14 10:05:56 +0800

    feat: rename app

--- ios/OtoRentTests/OtoRentTests.m
+++ ios/OtoRentTests/OtoRentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VehicelRentAppTests : XCTestCase
+@interface OtoRentTests : XCTestCase
 
 @end
 
-@implementation VehicelRentAppTests
+@implementation OtoRentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
