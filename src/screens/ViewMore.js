ef2cb68f0a2e3c24c9d5577200cd32213d31eb8d
Nabil Hardiansa <nhardiansa@gmail.com>
2022-05-13 21:31:55 +0800

    feat: activate viewmore feature

--- src/screens/ViewMore.js
+++ src/screens/ViewMore.js
@@ -1,13 +1,23 @@
-import React, {useLayoutEffect, useCallback} from 'react';
+import React, {useLayoutEffect, useCallback, useEffect, useState} from 'react';
 import {View, StyleSheet, SafeAreaView} from 'react-native';
 
-import {FlatList} from 'native-base';
+import {Box, FlatList} from 'native-base';
 
-import {MOTORBIKE_PLACEHOLDER} from '../assets/images';
+import {capitalize} from '../helpers/formatter';
 import ItemCard from '../components/ItemCard';
 import BackSection from '../components/BackSection';
+import LoadingScreen from '../components/LoadingScreen';
 
-export default function ViewMore({navigation}) {
+import {axiosInstance} from '../helpers/http';
+import qs from 'query-string';
+import {useDispatch, useSelector} from 'react-redux';
+import {VEHICLE_DETAIL} from '../helpers/destinationConstants';
+import {getVehicleDetail} from '../redux/actions/vehicleActions';
+
+export default function ViewMore({route, navigation}) {
+  const {type} = route.params;
+  const dispatch = useDispatch();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: '',
@@ -17,58 +27,81 @@ export default function ViewMore({navigation}) {
     });
   }, [navigation, goBack]);
 
+  const {vehicle: vehicleDetail, loading: vehicleDetailLoading} = useSelector(
+    state => state.vehiclesReducer,
+  );
+
+  const [vehicles, setVehicles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pageInfo, setPageInfo] = useState({});
+  const [onRefresh, setOnRefresh] = useState(false);
+
+  useEffect(() => {
+    switch (type) {
+      case 'car': {
+        getVehicleList(2);
+        break;
+      }
+      case 'motorbike': {
+        getVehicleList(3);
+        break;
+      }
+      case 'bike': {
+        getVehicleList(4);
+        break;
+      }
+      default:
+        goBack();
+        break;
+    }
+  }, [type, onRefresh]);
+
+  useEffect(() => {
+    if (vehicleDetail.id) {
+      navigation.navigate(VEHICLE_DETAIL);
+    }
+  }, [vehicleDetail]);
+
   const goBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
 
-  const goToDetail = () => {
-    console.log('Go to Detail');
+  const goToDetail = id => {
+    console.log('Go to Detail', id);
+    dispatch(getVehicleDetail(id));
   };
 
-  const data = [
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-    {
-      image: MOTORBIKE_PLACEHOLDER,
-      location: 'Makassar',
-      price: 250000,
-      title: 'Vespa Matic',
-      isAvailable: true,
-    },
-  ];
+  const getVehicleList = async (categoryId, loadMore = false) => {
+    try {
+      setLoading(true);
+      const params = {
+        category_id: categoryId,
+      };
+
+      let result;
+
+      if (loadMore) {
+        const {data} = await axiosInstance(null, false).get(pageInfo.nextPage);
+        data.results = [...vehicles, ...data.results];
+        result = data;
+      } else {
+        const {data} = await axiosInstance().get(
+          `/vehicles/filter?${qs.stringify(params)}`,
+        );
+        result = data;
+      }
+      setVehicles(result.results);
+      setPageInfo(result.pageInfo);
+      console.log(result.pageInfo);
+      setLoading(false);
+      setOnRefresh(false);
+      console.log('new data is coming');
+    } catch (err) {
+      console.error(err);
+      goBack();
+      setLoading(false);
+    }
+  };
 
   const styles = StyleSheet.create({
     container: {
@@ -79,13 +112,46 @@ export default function ViewMore({navigation}) {
   return (
     <SafeAreaView>
       <View>
-        <FlatList
-          contentContainerStyle={styles.container}
-          data={data}
-          renderItem={({item}) => {
-            return <ItemCard onPress={goToDetail} mb="4" image={item.image} />;
-          }}
-        />
+        {(loading && vehicles.length < 1) || vehicleDetailLoading ? (
+          <Box h="full" alignItems="center" justifyContent="center">
+            <LoadingScreen />
+          </Box>
+        ) : (
+          <>
+            {/* {vehicleDetailLoading ? (
+              <Box h="full" alignItems="center" justifyContent="center">
+                <LoadingScreen title="Getting Vehicle" />
+              </Box>
+            ) : (
+              <></>
+            )} */}
+            <FlatList
+              refreshing={onRefresh}
+              onRefresh={() => setOnRefresh(true)}
+              contentContainerStyle={styles.container}
+              data={vehicles}
+              onEndReachedThreshold={0.5}
+              onEndReached={() => {
+                if (pageInfo.nextPage) {
+                  getVehicleList(null, true);
+                }
+              }}
+              renderItem={({item}) => {
+                return (
+                  <ItemCard
+                    vehicleType={type}
+                    name={capitalize(item.name)}
+                    price={item.price}
+                    location={capitalize(item.location)}
+                    onPress={() => goToDetail(item.id)}
+                    mb="4"
+                    image={item.image}
+                  />
+                );
+              }}
+            />
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
